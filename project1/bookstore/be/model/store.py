701011ad48101c720df9a43179a6b8a36cc26708
LiuJiangTao <10225501445@stu.ecnu.edu.cn>
2024-10-28 21:32:34 +0800

    增添了部分注释

--- project1/bookstore/be/model/store.py
+++ project1/bookstore/be/model/store.py
@@ -14,28 +14,33 @@ class Store:
     def init_tables(self):
         try:
             conn = self.get_db_conn()
+            # 用户表： 用户id（主键） 密码 余额 token terminal
             conn.execute(
                 "CREATE TABLE IF NOT EXISTS user ("
                 "user_id TEXT PRIMARY KEY, password TEXT NOT NULL, "
                 "balance INTEGER NOT NULL, token TEXT, terminal TEXT);"
             )
 
+            # 店主-书店 联表： 用户id 书店id 两个作为 联合主键 
             conn.execute(
                 "CREATE TABLE IF NOT EXISTS user_store("
                 "user_id TEXT, store_id, PRIMARY KEY(user_id, store_id));"
             )
 
+            # 书店表： 书店id 书id  书信息 库存 联合主键（书店id,书id）
             conn.execute(
                 "CREATE TABLE IF NOT EXISTS store( "
                 "store_id TEXT, book_id TEXT, book_info TEXT, stock_level INTEGER,"
                 " PRIMARY KEY(store_id, book_id))"
             )
-
+            
+            # 订单表：订单id（主键） 买家id 书店id
             conn.execute(
                 "CREATE TABLE IF NOT EXISTS new_order( "
                 "order_id TEXT PRIMARY KEY, user_id TEXT, store_id TEXT)"
             )
-
+            
+            # 订单详情表：订单id 书id 数目 价格 
             conn.execute(
                 "CREATE TABLE IF NOT EXISTS new_order_detail( "
                 "order_id TEXT, book_id TEXT, count INTEGER, price INTEGER,  "
